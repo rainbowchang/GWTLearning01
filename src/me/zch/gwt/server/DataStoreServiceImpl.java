820ed8268ae9a3fc71622992d88555e22dff027b
rainbowchang <rainbow_chang@hotmail.com>
2016-06-03 16:50:53 +0800

    数据库代码练习

--- src/me/zch/gwt/server/DataStoreServiceImpl.java
+++ src/me/zch/gwt/server/DataStoreServiceImpl.java
@@ -3,13 +3,25 @@ package me.zch.gwt.server;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import me.zch.gwt.client.DataStoreService;
@@ -27,26 +39,35 @@ public class DataStoreServiceImpl extends RemoteServiceServlet implements DataSt
 		
 //		Entity e = new Entity(key);
 //		e.setProperty("Person_id", 1);
-//		e.setProperty("Firstname", "Tom");
-//		e.setProperty("Lastname", "Kissinger");
-//		e.setProperty("age", 44);
+//		e.setProperty("Firstname", "Jerry");
+//		e.setProperty("Lastname", "Moa");
+//		e.setProperty("age", 2);
 //		key = ds.put(e);
 //		
 //		Entity user = new Entity("User", key);
-//		user.setProperty("email", "Tom@mail.com");
+//		user.setProperty("email", "Jerry@mail.com");
 //		user.setProperty("address", "117, lunie avenue.");	
 //		ds.put(user);
 		
 		String s = "";
 //		try {
-//			Entity e = ds.get(key);
-//			s = (String)e.getProperty("Firstname");
-//		} catch (EntityNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
+//			Entity e1 = ds.get(key);
+//			s = (String)e1.getProperty("Firstname");
+//		} catch (EntityNotFoundException ex) {
+//			ex.printStackTrace();
 //		}
 
-		//Query q
+		
+		
+		Query q = new Query("Person");
+		Filter filter = new FilterPredicate("Firstname", FilterOperator.EQUAL, "Jerry");
+		q.setFilter(filter);
+		PreparedQuery pq = ds.prepare(q);
+		for(Entity el : pq.asIterable()){
+			s += el.getProperty("Firstname") + "." + el.getProperty("Lastname") + "  ";
+		}
+		
 		return s;
 	}
 }
+
